46eea6369ea126be399d8f09959fd3a7ded6d4de
jalero <jalero@cisco.com>
2024-03-01 18:40:57 +0100

    Change the *.json file cleaner to always leave the file empty after delete.

--- Tools/jsonFileCleaner.py
+++ Tools/jsonFileCleaner.py
@@ -2,20 +2,33 @@ import json
 import logging
 
 
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger("Cleaning Json Files")
+
+
 def cleanJsonFiles(file_name_input):
-    logger = logging.getLogger("Cleaning Json Files")
-    logger.info("Reading ticket from reminder_data.json file - STARTED.")
+    logger.info("Reading ticket from *.json file - STARTED.")
     """
     DOCSTRING HERE
     """
     if file_name_input:
         with open(file_name_input, 'w') as json_file:
-            json.dump('', json_file)
+            json_file.close()
             logger.info(f'Cleaned up {file_name_input}.json file - COMPLETED')
     else:
         file_name_list = ['parentMsgIds', 'reminder_data', 'tamPTOStatus']
         for file_name in file_name_list:
             file_name = f'Files/{file_name}.json'
             with open(file_name, 'w') as json_file:
-                json.dump({}, json_file)
+                json_file.close()
                 logger.info(f'Cleaned up {file_name}.json file - COMPLETED')
+
+
+if __name__ == '__main__':
+    file = None
+    cleanJsonFiles(file)
+
