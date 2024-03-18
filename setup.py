50007f25aa28a9a7cbe84fd5908070b2306f9ebe
jalero <jalero@cisco.com>
2024-03-18 09:02:50 +0100

    added setup.py file.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='tambacklogbuddy',
+setup(name='GlobalTAMQueueWatcher',
       author='Joshua Alero',
       version='1.0',
-      url='https://gitlab-sjc.cisco.com/jalero/tambacklogbuddy.git',
-      description='tambacklogbuddy',
+      url='https://gitlab-sjc.cisco.com/jalero/globaltamqueuewatcher.git',
+      description='GlobalTAMQueueWatcher',
       packages=find_packages('.'),
       install_requires=['python-dotenv',
                         'requests',
