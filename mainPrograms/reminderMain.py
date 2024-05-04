8192c01f9dc9a7bbb98d1aa57cffa8827b3925d3
jalero <jalero@cisco.com>
2024-05-04 22:31:58 +0200

    Added explicit weekend checks on the reminderMain.py file so that it doesn't execute the "else" statement after just one weekend data received.

--- mainPrograms/reminderMain.py
+++ mainPrograms/reminderMain.py
@@ -5,6 +5,7 @@ from tQwAlerter.shiftTimeDataClass import ShifttimeData as sd
 import logging
 import time
 from Tools.jsonFileCleaner import cleanJsonFiles
+from datetime import datetime
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s',
@@ -21,9 +22,13 @@ def runReminderService() -> None:
     logger.info("Running Reminder Service Program")
 
     while True:
-        weekend_data = sd().weekendAlertData()
+        # weekend_data = sd().weekendAlertData()
+        currentDateAndTime = datetime.now()
+        today = currentDateAndTime.strftime('%A')
         # Start clean up process once weekend data is received.
-        if weekend_data:
+        # if weekend_data:
+        if (today == "Saturday" and (currentDateAndTime.hour >= 2 and currentDateAndTime.minute >= 0)) or (
+                today == "Sunday"):
             logger.info(f"Weekend is here. Time to clean up database file. \n")
             reminder_file_path = "Files/reminder_data.json"
             is_cleaned = cleanJsonFiles(reminder_file_path)
@@ -35,6 +40,9 @@ def runReminderService() -> None:
                     reset_tam_assigned_tickets_stats()
                 if len(ret_is_assigned_msg_sent()) > 0:
                     reset_is_assigned_msg_sent()
+            else:
+                logger.info(f"Reminder json file already cleaned.")
+                logger.info(f"Weekend... resting...")
         else:
             createRmndrMsg(retTickFromDataList(read_json_file_line_by_line()))
             shift_data = sd().theatre_shift_time()
