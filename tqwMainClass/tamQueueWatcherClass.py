faaf8502842948b6728e3471682293c4e22b2be1
jalero <jalero@cisco.com>
2025-02-17 11:55:56 +0100

    Added new TSEs to availability alerter.

--- tqwMainClass/tamQueueWatcherClass.py
+++ tqwMainClass/tamQueueWatcherClass.py
@@ -150,7 +150,8 @@ class TamQueueWatcher:
         self.TSE_EMEA = ["roavrame@cisco.com", "mbignell@cisco.com", "beash@cisco.com", "cloupas@cisco.com",
                          "harpraja@cisco.com", "kloxdale@cisco.com", "rosahni@cisco.com", "rysia@cisco.com",
                          "bilbarne@cisco.com", "gonzamar@cisco.com",
-                         "arataj@cisco.com", "nucabral@cisco.com"]
+                         "arataj@cisco.com", "nucabral@cisco.com", "balka@cisco.com",
+                         "malnadi@cisco.com", "namro@cisco.com", "oghawi@cisco.com", "zalali@cisco.com"]
         self.TSE_Mngrs_EMEA = ["vesenthi@cisco.com", "dpetrezs@cisco.com"]
         self.TSE_TLs_EMEA = ["mbignell@cisco.com", "roavrame@cisco.com"]
 
